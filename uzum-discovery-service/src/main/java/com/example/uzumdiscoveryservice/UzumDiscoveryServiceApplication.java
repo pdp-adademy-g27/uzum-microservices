d306681f50a7753e52fd2190d0075af2e0cd5a3c
Abdurashidov <asilbekdev@gmail.com>
2024-01-30 10:48:29 +0500

    Initial commit

---
+++ uzum-discovery-service/src/main/java/com/example/uzumdiscoveryservice/UzumDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.uzumdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UzumDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UzumDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ uzum-discovery-service/src/test/java/com/example/uzumdiscoveryservice/UzumDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uzumdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UzumDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
